ad84d4a3ba0e18961487426132e33f3d0eece30a
TomKalva <tomkalva@seznam.cz>
2021-08-16 18:43:27 +0200

    Staggering now correctly plays its animation.

--- Assets/Characters/Acts/StaggeredAct.cs
+++ Assets/Characters/Acts/StaggeredAct.cs
@@ -15,6 +15,8 @@ public class StaggeredAct : AnimatedAct
 
     public override void OnStart(Agent agent)
     {
+        PlayAnimation(agent);
+
         agent.movement.Impulse(PushForce);
 
         agent.movement.VelocityUpdater = new DontChangeVelocityUpdater(duration);
