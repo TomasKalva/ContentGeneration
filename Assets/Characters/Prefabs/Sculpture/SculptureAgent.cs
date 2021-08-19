ad818c8ec912728fbbe85ff1fd9440747d223772
TomKalva <tomkalva@seznam.cz>
2021-08-19 18:44:43 +0200

    Sculpture now has cool attacks.

---
+++ Assets/Characters/Prefabs/Sculpture/SculptureAgent.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SculptureAgent : Agent
+{
+    public void OverheadAttack()
+    {
+        ResetState();
+        acting.SelectAct("Overhead");
+    }
+
+    public void WideAttack()
+    {
+        ResetState();
+        var currentAct = acting.ActiveAct;
+
+        // do a combo if slash is currently active
+        Attack attack;
+        if (currentAct && currentAct.actName == "LeftWide")
+        {
+            attack = acting.SelectAct("RightWideDown") as Attack;
+        }
+        else
+        {
+            attack = acting.SelectAct("LeftWide") as Attack;
+        }
+        attack.Direction = movement.AgentForward;
+    }
+}
--- Assets/Characters/Prefabs/Sculpture/SculptureController.cs
+++ Assets/Characters/Prefabs/Sculpture/SculptureController.cs
@@ -2,7 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Agent))]
-public class SculptureController : EnemyController<Agent>
+[RequireComponent(typeof(SculptureAgent))]
+public class SculptureController : EnemyController<SculptureAgent>
 {
+	[SerializeField]
+	ColliderDetector overheadArea;
+
+	void Awake()
+	{
+		agent = GetComponent<SculptureAgent>();
+	}
+
+	protected override void UpdateController(Vector2 movementDirection)
+	{
+		// check if my attack hits enemy
+		if (overheadArea.Triggered)
+		{
+			var hitAgent = overheadArea.other.GetComponentInParent<Agent>();
+			if (hitAgent != agent)
+			{
+				agent.WideAttack();
+			}
+		}
+	}
 }
