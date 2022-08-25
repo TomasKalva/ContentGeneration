7bc6059b8a1fd83bffa7790ce058abd1e2d31af5
TomKalva <tomkalva@seznam.cz>
2022-08-25 15:13:45 +0200

    * replaced all enemy controller with EnemyController.

--- Assets/Characters/EnemyController.cs
+++ Assets/Characters/EnemyController.cs
@@ -5,55 +5,28 @@ using UnityEngine;
 using static Movement;
 
 [RequireComponent(typeof(Agent))]
-public abstract class EnemyController<AgentT> : MonoBehaviour where AgentT : Agent
+public class EnemyController : MonoBehaviour
 {
-	protected AgentT agent;
-
-
+	Agent agent;
 
 	// Start is called before the first frame update
 	void Awake()
 	{
-		agent = GetComponent<AgentT>();
+		agent = GetComponent<Agent>();
 	}
 
     private void Start()
     {
-		AddBehaviors(agent.Behaviors);
 		agent.acting.MyReset();
 	}
-
-    public virtual void AddBehaviors(Behaviors behaviors) { }
-
-	void OnWorldCreated()
-	{
-    }
 
     // Update is called once per frame
     void FixedUpdate()
 	{
 		agent.StartReceivingControls();
 
-		/*Vector3 direction = TargetPoint - agent.movement.body.position;
-		Vector2 movementDirection = new Vector2(direction.x, direction.z);
-		movementDirection = Vector2.ClampMagnitude(movementDirection, 1f);
-		if (GoToTarget())
-		{
-			agent.Run(movementDirection);
-		}
-		else
-		{
-			agent.Turn(movementDirection);
-		}
-
-		UpdateController(movementDirection);
-		*/
 		agent.Behaviors.UpdateBehavior(agent);
 
 		agent.UpdateAgent();
 	}
-	/*
-	protected virtual void UpdateController(Vector2 movementDirection)
-	{
-	}*/
 }
